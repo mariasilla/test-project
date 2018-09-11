0b0aabfbebe61b0c147ea78011c8e97438d65af5
Silla <maria.sylla@gmail.com>
2018-09-11 16:25:53 +0300

    add mat-app-background class to body

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(@Inject(DOCUMENT) document, r: Renderer2) {
+    r.addClass(document.body, 'mat-app-background');
+  }
   title = 'Tour of Heroes';
 }
