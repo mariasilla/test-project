c06a1207c5370d6b57a845c037ed35e5d01e36b8
Silla <maria.sylla@gmail.com>
2018-09-18 14:24:08 +0300

    add simple-hex-to-rgb converter

--- cypress/integration/buttons/checkBtnColor.js
+++ cypress/integration/buttons/checkBtnColor.js
@@ -1,10 +1,13 @@
+const convert = require('simple-hex-to-rgb');
+
 describe('checkBtnColor', function() {
   it('has color #b00020', function() {
+    const errorPaletteColor = convert("#b00020");
     cy.visit('http://localhost:4200/heroes');
     cy.get('.mat-app-background')
       .find('button')
       .click()
       .should('have.css', 'background-color')
-      .and('eq', '#b00020');
+      .and('eq', errorPaletteColor);
   });
 });
