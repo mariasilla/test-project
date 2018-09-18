69e4355ab51d62891cc7b80d4a7ed6d40f064999
Silla <maria.sylla@gmail.com>
2018-09-18 12:28:04 +0300

    add checkIfErrorPalette.js test

---
+++ cypress/integration/buttons/checkIfErrorPalette.js
@@ -0,0 +1,20 @@
+describe('checkIfErrorPaletteExists', function() {
+  it('button error color attribute', function() {
+    cy.visit('http://localhost:4200/heroes');
+    cy.get('.mat-app-background')
+      .find('button')
+      .click()
+      .should('have.attr', 'color', 'error')
+  });
+});
+
+
+// describe('checkIfErrorPaletteExists', function() {
+//   it('button has .mat-error class', function() {
+//     cy.visit('http://localhost:4200/heroes');
+//     cy.get('.mat-app-background')
+//       .find('button')
+//       .click()
+//       .and('have.class', '.mat-raised-button.mat-error');
+//   });
+// });
